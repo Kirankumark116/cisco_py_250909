3717790888b8db72b390a849bc51f6fcd8dec2ef
Kiran <kirankumark.ise.rymec@gmail.com>
2025-09-11 11:39:22 +0530

    Day 3 Sql

--- Day 2/question4.py
+++ Day 2/question4.py
@@ -1,2 +1,13 @@
 words_str=input("sentence: ")
-sentence_list=[words.sort() for words in words_str.split()]
+sentence_list=[words for words in words_str.split()]
+sentence_list.sort()
+print(sentence_list)
+
+file_name='Names_data.txt'
+with open(file_name,'w') as writer:
+    writer.write(f'List:{sentence_list}\n')
+
+with open(file_name,'r') as reader:
+    line_list=reader.readline()
+    print(line_list)
+
---
+++ Day 2/question5.py
@@ -0,0 +1,3 @@
+numbers_str=input("numbers (space separeted):")
+numbers_list=[int(num_str) for num_str in numbers_str.split()]
+print(numbers_list)
